4ccff6dc724803b2470dc072e5caf60a3657cbc7
weiyang <weiyang.ones@gmail.com>
2017-10-05 12:57:23 +0800

    [installer] Add 'golite_doctor' command
    Signed-off-by: weiyang <weiyang.ones@gmail.com>

--- golite/installer.py
+++ golite/installer.py
@@ -3,6 +3,16 @@ import subprocess
 import sublime
 import sublime_plugin
 
+from . import utils
+
+go_tools = {
+    "gocode": "github.com/nsf/gocode",
+    "guru": "golang.org/x/tools/cmd/guru",
+    "goimports": "golang.org/x/tools/cmd/goimports",
+    "godef": "github.com/rogpeppe/godef",
+    "gometalinter": "github.com/alecthomas/gometalinter",
+}
+
 
 class GoliteInstallCommand(sublime_plugin.ApplicationCommand):
     """GoliteInstallCommand
@@ -12,13 +22,6 @@ class GoliteInstallCommand(sublime_plugin.ApplicationCommand):
     Extends:
         sublime_plugin.ApplicationCommand
     """
-    go_tools = {
-        "gocode": "github.com/nsf/gocode",
-        "guru": "golang.org/x/tools/cmd/guru",
-        "goimports": "golang.org/x/tools/cmd/goimports",
-        "godef": "github.com/rogpeppe/godef",
-        "gometalinter": "github.com/alecthomas/gometalinter",
-    }
 
     def run(self):
         sublime.set_timeout_async(self.install_go_tools, 0)
@@ -28,9 +31,9 @@ class GoliteInstallCommand(sublime_plugin.ApplicationCommand):
         auto_update = settings.get("auto_update_go_tools", False)
 
         print("[golite] start installing go tools: [%s]" %
-              ','.join(self.go_tools.keys()))
-        for tool in self.go_tools:
-            args = ["go", "get", self.go_tools[tool]]
+              ','.join(go_tools.keys()))
+        for tool in go_tools:
+            args = ["go", "get", go_tools[tool]]
             if auto_update:
                 args.insert(2, "-u")
             self.run_cmd(args, timeout=60)
@@ -45,3 +48,53 @@ class GoliteInstallCommand(sublime_plugin.ApplicationCommand):
     def run_cmd(self, args, timeout=60):
         print("[golite] running '%s'" % ' '.join(args))
         subprocess.check_call(args, timeout=timeout)
+
+
+class GoliteDoctorCommand(sublime_plugin.ApplicationCommand):
+    """GoliteDoctorCommand
+
+    Audits installation for common issues
+
+    Extends:
+        sublime_plugin.ApplicationCommand
+    """
+
+    def run(self):
+        self.doctor()
+
+    def doctor(self):
+        msgs = []
+
+        # check golang
+        msg = "[%s] Golang installed"
+        installed = True
+        try:
+            utils.executable_path("go", window=sublime.active_window())
+        except EnvironmentError:
+            installed = False
+        msgs.append(msg % ("x" if installed else "  "))
+
+        # check all go tools have been installed
+        msg = "[%s] Go tools installed"
+        installed = True
+        for tool in go_tools.keys():
+            try:
+                utils.executable_path("go", window=sublime.active_window())
+            except EnvironmentError:
+                installed = False
+                break
+        msgs.append(msg % ("x" if installed else "  "))
+
+        # check sublimelinter
+        msg = "[%s] Sublimelinter installed"
+        installed = True
+        try:
+            from SublimeLinter.lint import Linter
+        except ImportError:
+            installed = False
+        msgs.append(msg % ("x" if installed else "  "))
+
+        if sublime.ok_cancel_dialog("%s" % '\n'.join(msgs),
+                                    'Open Documentation'):
+            sublime.run_command('open_url',
+                                {'url': 'https://github.com/wy-z/Golite'})
--- main.py
+++ main.py
@@ -1,56 +1,34 @@
 import sublime
-import golangconfig
 
 from .golite.formatter import GoliteFormatCommand, GoliteFormatListener
 from .golite.gocode import GocodeListener
 from .golite.godef import GoliteGodefCommand
-from .golite.installer import GoliteInstallCommand
+from .golite.installer import GoliteDoctorCommand, GoliteInstallCommand
 from .golite.linter import Gometalinter
 
 
 def plugin_loaded():
-    golite_check()
     settings = sublime.load_settings("Golite.sublime-settings")
     if settings.get("auto_update_go_tools", False):
         sublime.run_command("golite_install")
+    check_prompt()
 
 
-def golite_check():
+def check_prompt():
     settings = sublime.load_settings("Golite.sublime-settings")
     if settings.get("prompted", False):
         return
 
-    # check go env
-    try:
-        golangconfig.subprocess_info(
-            'go', ["GOPATH", "GOAP"], window=sublime.active_window())
-    except Exception as e:
-        prompt(e)
-
-    # check sublimeLinter
-    try:
-        import SublimeLinter
-    except ImportError as e:
-        msg = "SublimeLinter is required for code linting"
-        prompt(e)
-
-    # try to install go
+    # try to install go tools
     sublime.run_command("golite_install")
 
     # set 'auto_update_go_tools'
     return_code = sublime.yes_no_cancel_dialog(
-        "[Golite] Do you want to auto update go tools.")
+        "[Golite] Do you want to auto update go tools?")
     if return_code == sublime.DIALOG_YES:
         settings.set("auto_update_go_tools", True)
-    else:
+    elif return_code == sublime.DIALOG_NO:
         settings.set("auto_update_go_tools", False)
 
     settings.set("prompted", True)
     sublime.save_settings("Golite.sublime-settings")
-
-
-def prompt(err_msg):
-    if sublime.ok_cancel_dialog("[Golite] %s." % err_msg,
-                                'Open Documentation'):
-        sublime.run_command('open_url',
-                            {'url': 'https://github.com/wy-z/Golite'})
