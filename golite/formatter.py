5adf2731332028852827d8a79da5adc531257ba7
weiyang <weiyang.ones@gmail.com>
2017-10-11 15:19:03 +0800

    [formatter] Format temporary file
    Signed-off-by: weiyang <weiyang.ones@gmail.com>

--- golite/formatter.py
+++ golite/formatter.py
@@ -1,4 +1,6 @@
+import os
 import subprocess
+import tempfile
 
 import sublime
 import sublime_plugin
@@ -20,7 +22,7 @@ class GoliteFormatCommand(sublime_plugin.TextCommand):
             formatted = False
             if mode in ["goimports", "both"]:
                 try:
-                    code = self.fromat("goimports", code, edit)
+                    self.fromat("goimports", code, edit)
                     formatted = True
                 except Exception as e:
                     print(
@@ -30,14 +32,14 @@ class GoliteFormatCommand(sublime_plugin.TextCommand):
                 args = []
                 if settings.get("gofmt_simplified", False):
                     args = ["-s"]
-                code = self.fromat("gofmt", code, edit, args)
+                self.fromat("gofmt", code, edit, args)
         except Exception:
             self.view.set_read_only(False)
             raise
         finally:
             self.view.set_read_only(False)
 
-    def fromat(self, formatter, code, edit, args=[]):
+    def fromat(self, formatter, code, edit, args=None):
         """fromat
 
         Run formatter and modify the buffer if needed
@@ -48,7 +50,7 @@ class GoliteFormatCommand(sublime_plugin.TextCommand):
             edit      {sublime.Edit} -- sublime edit instance
 
         Keyword Arguments:
-            args {list} -- [description] (default: {[]})
+            args {list} -- [description] (default: {None})
 
         Returns:
             string -- formatted code
@@ -56,27 +58,35 @@ class GoliteFormatCommand(sublime_plugin.TextCommand):
         Raises:
             RuntimeError -- raise runtimeError when processing failed
         """
-        args.insert(0, formatter)
-        proc = subprocess.Popen(
-            args,
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            env=utils.get_env(),
-            startupinfo=utils.get_startupinfo(),
-            shell=True)
-        out, err = proc.communicate(input=code.encode("utf-8"))
-        if proc.returncode != 0:
-            raise RuntimeError(err.decode('utf-8'))
+        if args is None:
+            args = []
 
-        out_str = ""
-        if out is not None and out != b'':
-            out_str = out.decode('utf-8')
-            if code != out_str:
-                region = sublime.Region(0, self.view.size())
-                self.view.set_read_only(False)
-                self.view.replace(edit, region, out_str)
-        return out_str
+        with tempfile.NamedTemporaryFile(
+                dir=os.path.dirname(self.view.file_name())) as tmp:
+            tmp.write(code.encode("utf-8"))
+            tmp.flush()
+            file = tmp.name
+
+            args.insert(0, formatter)
+            args.append(file)
+            proc = subprocess.Popen(
+                args,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                env=utils.get_env(),
+                startupinfo=utils.get_startupinfo())
+            out, err = proc.communicate()
+            if proc.returncode != 0:
+                raise RuntimeError(err.decode('utf-8'))
+
+            out_str = ""
+            if out is not None and out != b'':
+                out_str = out.decode('utf-8')
+                if code != out_str:
+                    region = sublime.Region(0, self.view.size())
+                    self.view.set_read_only(False)
+                    self.view.replace(edit, region, out_str)
+            return out_str
 
 
 class GoliteFormatListener(sublime_plugin.EventListener):
